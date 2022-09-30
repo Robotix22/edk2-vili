88aad0ff7cddfd9881909d20db55f8cda32d3bc5
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2022-09-30 16:30:33 +0200

    Fix Boot Image Build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ rm -rf Build/viliPkg/DEBUG_GCC5/FV/Ffs/7E374E25-8E01-4FEE-87F2-390C23C606CDFVMAI
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p viliPkg/vili.dsc -b DEBUG
 gzip -c < Build/viliPkg/DEBUG_GCC5/FV/VILIPKG_UEFI.fd >uefi_image
 cat vili.dtb >>uefi_image
-mkbootimg --kernel uefi_image --ramdisk ramdisk --kernel_offset 0x00000000 --ramdisk_offset 0x00000000 --tags_offset 0x00000000 --os_version 12.0.0 --os_patch_level "$(date '+%Y-%m')" --header_version 1 -o boot-vili.img
+mkbootimg --kernel uefi_image --ramdisk ramdisk --kernel_offset 0x00000000 --ramdisk_offset 0x00000000 --tags_offset 0x00000000 --os_version 12.0.0 --os_patch_level "$(date '+%Y-%m')" -o boot-vili.img
